6bc0d6afdcdb25f56e4131883f4a54aa07129ed2
unknown <ishwar.rimal@gmail.com>
2020-05-20 20:09:26 +0530

    updated variables name

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,10 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 async function gotMessage(noOfUser, sender, sendresponse) {
   console.log({ noOfUser });
-  var val = document.querySelectorAll('[data-control-name="withdraw_single"]');
-  var len = noOfUser || val.length;
+  var totalConnections = document.querySelectorAll(
+    '[data-control-name="withdraw_single"]'
+  );
+  var len = noOfUser || totalConnections.length;
   var i = 0;
   var to;
   alert(len);
@@ -13,9 +15,9 @@ async function gotMessage(noOfUser, sender, sendresponse) {
       clearInterval(to);
       return;
     }
-    var id = val[i].id;
+    var id = totalConnections[i].id;
     console.log({ id });
-    val[i].click();
+    totalConnections[i].click();
     to = setTimeout(() => {
       console.log("removeing inner");
       var elem = document.querySelector("[data-test-dialog-primary-btn]");
