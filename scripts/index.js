65e79082f2f69640bc502c56852e5fd85f1b9d8a
Shruthi Kulkarni <shruthip15.sk@gmail.com>
2020-07-04 16:00:54 +0530

    removed from last with page navigation bug fixed

--- scripts/index.js
+++ scripts/index.js
@@ -6,31 +6,45 @@ async function gotMessage(inputObj, sender, sendresponse) {
   if (inputObj.type === "redirect") {
     window.location.replace(connectionUrl);
   } else if (inputObj.type === "withdraw") {
-    var totalConnections = document.querySelectorAll(
-      '[data-control-name="withdraw_single"]'
-    );
+    
     const olderFirst = inputObj.order === "old";
     goToLastPage(olderFirst);
-    var totalLength = totalConnections.length;
-    var noOfUsers = inputObj.noOfUser || totalLength;
-    var startCounter = olderFirst ? totalLength - 1 : 0;
-    var counter = 0;
-    var waitForClick;
-    var syncIt = () => {
-      if (counter > Number(noOfUsers)) {
-        clearInterval(waitForClick);
-        return;
-      }
-      totalConnections[startCounter].click();
-      waitForClick = setTimeout(() => {
-        var elem = document.querySelector("[data-test-dialog-primary-btn]");
-        elem.click();
-        startCounter = olderFirst ? startCounter - 1 : startCounter + 1;
-        counter++;
-        syncIt();
-      }, 700);
-    };
-    syncIt();
+    setTimeout(() => {
+      var totalConnections = document.querySelectorAll(
+        '[data-control-name="withdraw_single"]'
+      );
+      var totalLength = totalConnections.length;
+      var noOfUsersToWithdraw = inputObj.noOfUser || totalLength;
+      var startCounter = olderFirst ? totalLength - 1 : 0;
+      var removeCounter = 0;
+      var refNumber = olderFirst ? totalLength - 1 : 0;
+      var waitForClick;
+
+      var syncIt = () => {
+        if (document.readyState !== 'loading') {
+          if (removeCounter > Number(noOfUsersToWithdraw)) {
+            clearInterval(waitForClick);
+            return;
+          }
+          totalConnections[startCounter].click();
+          waitForClick = setTimeout(() => {
+            var elem = document.querySelector("[data-test-dialog-primary-btn]");
+            elem.click();
+            startCounter = olderFirst ? startCounter - 1 : startCounter + 1;
+            removeCounter++; // 1
+            if (olderFirst && refNumber < removeCounter - 1) {
+              console.log("last page navigated");
+              goToLastPage(olderFirst);
+            }
+            if (removeCounter < Number(noOfUsersToWithdraw)) {
+              syncIt();
+            }
+          }, 700);
+        }
+        
+      };
+      syncIt();
+    }, 2000);
   }
 
   function goToLastPage(olderFirst) {
